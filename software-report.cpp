f8523cf9189e903465d2181bb7cbd242e4676bed
Naoki Ueda <su104003@gmail.com>
2015-01-27 23:18:32 +0900

    ---

---
+++ software-report.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// 食料品を扱うクラス
+class Grocery {
+  public:
+    Grocery();
+  private:
+    string groceries;
+};
+
+// 伝票を作成するクラス
+class SlipManager {
+
+};
+
+// 顧客の情報を管理するクラス
+class ClientInfoManager {
+
+};
+
+int main() {
+  return 0;
+}
