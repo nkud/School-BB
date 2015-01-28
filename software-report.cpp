fb5f92f7b82e1dc00932f878b579754c502a7935
Naoki Ueda <su104003@gmail.com>
2015-01-28 12:20:27 +0900

    ---

--- software-report.cpp
+++ software-report.cpp
@@ -2,23 +2,32 @@
 using namespace std;
 
 // 食料品を扱うクラス
-class Grocery {
+class GroceryManager {
   public:
-    Grocery();
+    GroceryManager() { cout << "Initialize Grocery Manager." << endl; }
   private:
     string groceries;
 };
 
 // 伝票を作成するクラス
 class SlipManager {
-
+  public:
+    SlipManager() { cout << "Initialize Slip Manager" << endl; }
+  private:
 };
 
 // 顧客の情報を管理するクラス
 class ClientInfoManager {
-
+  public:
+    ClientInfoManager() { cout << "Initialize Client Manager" << endl; }
+  private:
 };
 
 int main() {
+  cout << "バーチャルマーケットシステム" << endl;
+  GroceryManager groceryManage;
+  SlipManager slipManager;
+  ClientInfoManager clientManger;
+
   return 0;
 }
