ab477466e0af7a88aad5ce8b5ad58e1aba23ec6a
Naoki Ueda <su104003@gmail.com>
2015-01-28 13:53:59 +0900

    ---

--- software-report.py
+++ software-report.py
@@ -1,7 +1,11 @@
 #! /usr/bin/python
 # coding=utf-8
 
+### データ
+# 商品のカテゴリー
 CATEGORY = [ '野菜', '肉', '魚', '果物', '乳製品', '冷凍食品', 'パン', '米', '弁当', 'その他' ]
+
+# 各カテゴリーの商品
 GROCERY = [ 
 [['Yasai1', '100yen', 'Mr.A', '0'],['Yasai2', '100yen', 'Mr.A', '0'],['Yasai3', '100yen', 'Mr.A', '0']],
 [['Niku1', '100yen', 'Mr.A', '0'],['Niku2', '100yen', 'Mr.A', '0'],['Niku3', '100yen', 'Mr.A', '0']],
@@ -15,32 +19,72 @@ GROCERY = [
 [['Sonota1', '100yen', 'Mr.A', '0'],['Sonota2', '100yen', 'Mr.A', '0'],['Sonota3', '100yen', 'Mr.A', '0']],
 ]
 
-# クラスの定義
+# 配送可能な地域
+AREA = [ 'area-A', 'area-B', 'area-C']
+
+### クラスの定義
+# 食料品を管理するクラス
 class GroceryManager(object):
     def __init__(self):
         """ 初期化する """
-        print '(食料品管理システム初期化)'
         self.category = CATEGORY
         self.grocery = GROCERY
-    def stockWithNumber(num):
-        """ 在庫を返す """
-        return self.stock[num]
+
     def showTable(self):
+        """ 商品メニューを表示する """
         print '---------------- CATEGORY'
         n = 0
         for c in self.category:
-            print '[%d] %s' % (n, c)
+            print '- [%d] %s' % (n, c)
             n += 1
         print '----------------'
 
+    def showStockWithNumber(self, num):
+        """ 在庫を表示する """
+        groceries = self.grocery[num]
+        for g in groceries:
+            print '- [商品名] %s | [価格] %s | [生産者]　%s | [在庫量] %s' % (g[0], g[1], g[2], g[3])
 
-class SlipManager():
+# 伝票を管理するクラス
+class SlipManager(object):
     def __init__(self):
-        print '(伝票管理システム初期化)'
+        pass
 
-class ClientInfoManager():
+# 顧客情報を管理するクラス
+class ClientInfoManager(object):
     def __init__(self):
-        print '(顧客管理システム初期化)'
+        self.client_data = []
+
+    def appendNewClient(self, name, address, tel, mail):
+        """ 新しい顧客を登録する """
+        clientdata = [name, address, tel, mail]
+        self.client_data.append(clientdata)
+
+    def inputClientInformation(self):
+        """ 氏名、住所、電話番号、メールアドレスを入力する """
+        print ">>> 登録開始"
+        name = ''
+        address = ''
+        tel = ''
+        mail = ''
+        while True:
+            name = raw_input("[ name ] ")
+            address = raw_input("[ address ] ")
+            tel = raw_input("[ tel ] ")
+            mail = raw_input("[ mail ] ")
+            print '----------------'
+            print('- 氏名: %s\n- 住所: %s\n- 電話番号: %s\n- E-mail: %s') % (name, address, tel, mail)
+            print '----------------'
+            ok = raw_input(">>> OK?(y/n) ")
+            if ok == 'y':
+                break
+        self.appendNewClient(name, address, tel, mail)
+        print ">>> 登録完了"
+
+# 入出力を管理するクラス
+class ViewController(object):
+    def __init__(self):
+        pass
 
 # メイン
 if __name__ == '__main__':
@@ -50,3 +94,5 @@ if __name__ == '__main__':
     clientInfoManager = ClientInfoManager()
 
     groceryManager.showTable()
+    groceryManager.showStockWithNumber(0)
+    clientInfoManager.inputClientInformation()
